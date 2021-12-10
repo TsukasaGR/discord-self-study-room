ece4e6a7fb86526af62a59ef401f0ddfd9f8326f
Tsukasa Sekiguchi <t14159265358979323846@gmail.com>
2021-12-10 16:40:27 +0900

    テスト

--- commands/studyStart.js
+++ commands/studyStart.js
@@ -10,34 +10,34 @@ const handle = async (client, interaction) => {
   const userName = interaction.member.user.username;
   const ssUrl = process.env.SS_URL;
 
-  // const res = await spreadSheetApis
-  //   .studyStart(at, userId, userName, ssUrl)
-  //   .catch((e) => {
-  //     return discord.responseInteraction(
-  //       client,
-  //       interaction,
-  //       `Whoops...\nバグってるので${ADMIN_USER_NAME}まで連絡ください...`
-  //     );
-  //   });
-  // if (res.status === "error") {
-  //   if (res.errorType === "UnregisteredUser") {
-  //     return discord.responseInteraction(
-  //       client,
-  //       interaction,
-  //       `ユーザー登録されていないようです。\n以下スプシの「ユーザーリスト」シートにユーザーを追加してください。\nあなたのDiscord IDは ${userId} です。\n${process.env.SS_URL_SHEET_DB}`
-  //     );
-  //   }
-  //   return discord.responseInteraction(
-  //     client,
-  //     interaction,
-  //     `自習開始に失敗しました。\n未終了のレコードがないかスプシを確認してください。\n(未終了を忘れた場合は手動で入力してください)\n${process.env.SS_URL_SHEET_DB}`
-  //   );
-  // }
+  const res = spreadSheetApis
+    .studyStart(at, userId, userName, ssUrl)
+    .catch((e) => {
+      return discord.responseInteraction(
+        client,
+        interaction,
+        `Whoops...\nバグってるので${ADMIN_USER_NAME}まで連絡ください...`
+      );
+    });
+  if (res.status === "error") {
+    if (res.errorType === "UnregisteredUser") {
+      return discord.responseInteraction(
+        client,
+        interaction,
+        `ユーザー登録されていないようです。\n以下スプシの「ユーザーリスト」シートにユーザーを追加してください。\nあなたのDiscord IDは ${userId} です。\n${process.env.SS_URL_SHEET_DB}`
+      );
+    }
+    return discord.responseInteraction(
+      client,
+      interaction,
+      `自習開始に失敗しました。\n未終了のレコードがないかスプシを確認してください。\n(未終了を忘れた場合は手動で入力してください)\n${process.env.SS_URL_SHEET_DB}`
+    );
+  }
 
-  return discord.responseInteraction(
+  discord.responseInteraction(
     client,
     interaction,
-    `自習スタートしました！`
+    `自習スタートの処理を受け取りました！`
   );
 };
 
